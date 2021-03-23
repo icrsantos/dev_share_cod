e8b7f6b83c59ffd6aaf1fd6772e4f72bef83fafa
Philemon da Silva Souza <Philemon.souza@supermix.com.br>
2021-03-23 13:37:56 -0300

    Melhora de Logs em Logger.py

--- src/Utils/Logger.py
+++ src/Utils/Logger.py
@@ -1,6 +1,11 @@
+import datetime
+
+
 def info(texto, classe):
-    print('(Classe: ' + str(classe) + ') \n ' + texto)
+    log_header = '[' + str(datetime.datetime.now()) + ' | ' + str(classe) + ']: '
+    print(log_header + texto)
 
 
 def erro(texto, excecao, classe):
-    print('(Classe: ' + str(classe) + ') \n ' + texto + ': ' + str(excecao))
+    log_header = '[' + str(datetime.datetime.now()) + ' | ' + str(classe) + ']: '
+    print(log_header + texto + ': ' + str(excecao))
