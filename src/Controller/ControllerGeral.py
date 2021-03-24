73bdec4f8f34fc8593818012e2cf9c8d4def165c
Philemon da Silva Souza <Philemon.souza@supermix.com.br>
2021-03-24 19:42:31 -0300

    Remoção de variáveis redundantes.

--- src/Controller/ControllerGeral.py
+++ src/Controller/ControllerGeral.py
@@ -10,22 +10,19 @@ CORS(app)
 @app.route('/api/usuario', methods=['GET'])
 def validar_usuario():
     usuario_json = request.json
-    LoginServico.validar_usuario(usuario_json)
     return str(LoginServico.validar_usuario(usuario_json))
 
 
 @app.route('/api/usuario', methods=['POST'])
 def criar_usuario():
     usuario_json = request.json
-    id_usuario_criado = LoginServico.criar_usuario(usuario_json)
-    return id_usuario_criado
+    return LoginServico.criar_usuario(usuario_json)
 
 
 @app.route('/api/postagem', methods=['POST'])
 def criar_postagem():
     postagem_json = request.json
-    id_postagem_criada = PostagemServico.criar_postagem(postagem_json)
-    return id_postagem_criada
+    return PostagemServico.criar_postagem(postagem_json)
 
 
 @app.route('/api/pesquisar/<pesquisa>', methods=['GET'])
