6367691ad5c01c7e71bccea9bcf9e7b0257deed5
Philemon da Silva Souza <Philemon.souza@supermix.com.br>
2021-04-06 21:07:33 -0300

    Criação de um teste de unidade simples para envio direto de e-mails.

--- src/Testes/NotificacaoServicoTeste.py
+++ src/Testes/NotificacaoServicoTeste.py
@@ -7,19 +7,8 @@ class NotificacaoServicoTeste(unittest.TestCase):
 
     def testar_disparo_email_simples(self):
         email_destinatario = 'philemon.silva@gmail.com'
-        mensagem = """Subject: Hi there
-        
-        
-        This message is sent from Python."""
-        resultado = NotificacaoServico.enviar_email(email_destinatario, mensagem)
-        self.assertEqual(True, resultado)
-
-    def testar_disparo_notificacao_resposta(self):
-        email_destinatario = 'philemon.silva@gmail.com'
-        mensagem = """Subject: Hi there
-
-
-        This message is sent from Python."""
+        mensagem = "Subject: Teste Unitário DevShare\n\n" \
+                   "Essa é uma mensagem de teste proveniênte do teste unitário da Devshare"
         resultado = NotificacaoServico.enviar_email(email_destinatario, mensagem)
         self.assertEqual(True, resultado)
 
