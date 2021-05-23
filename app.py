a7b942794f52223a33524e054fa62eca1554c667
Philemon da Silva Souza <Philemon.souza@supermix.com.br>
2021-05-23 19:51:33 -0300

    Corrigindo erro de compilação.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from src.Servico import LoginServico, PostagemServico, NotificacaoServico, CurtidasServico
+from src.Servico import LoginServico, PostagemServico, NotificacaoServico, CurtidasServico, EcoPostagemServico
 
 app = Flask(__name__)
 CORS(app)
