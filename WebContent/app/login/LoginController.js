1bbb905d41fe4cccbe9b1d33e53139476739fbd8
Philemon da Silva Souza <Philemon.souza@supermix.com.br>
2021-05-24 20:17:52 -0300

    Corrigindo bug onde o login era realizado mesmo sem a validação do código.

--- WebContent/app/login/LoginController.js
+++ WebContent/app/login/LoginController.js
@@ -1,15 +1,18 @@
-app.controller("LoginController", function(DevShareService, $state, Auth) {
+app.controller("LoginController", function (DevShareService, $state, Auth) {
     this.data = {};
-	this.processando = false;
+    this.processando = false;
 
-    this.save = function() {
+    this.save = function () {
         this.processando = true;
         DevShareService.objRest.one('/login').customPOST(this.data)
-        .then((response) => {
-            Auth.setUser(response.data);
-            $state.go("home");
-        }).finally(() => {
-            this.processando = false;
-        });
+            .then((response) => {
+                if (response.data !== 'False') {
+                    Auth.setUser(response.data);
+                    $state.go("home");
+                }
+            })
+            .finally(() => {
+                this.processando = false;
+            });
     }
 });
