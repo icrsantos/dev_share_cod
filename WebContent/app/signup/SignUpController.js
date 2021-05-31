a0e50e74aa7ca52042ecc3a79b98490af6dac7d6
Isabela Santos <icrsantos@sga.pucminas.br>
2021-05-31 19:18:33 -0300

    coloca oauth no cadastro

--- WebContent/app/signup/SignUpController.js
+++ WebContent/app/signup/SignUpController.js
@@ -1,7 +1,18 @@
 app.controller('SignUpController', function($rootScope, DevShareService, $state) {
 	this.data = {};
 	this.processando = false;
-	
+
+	this.$onInit = () => {
+        gapi.signin2.render('loginGmail', {
+            'scope': 'email profile https://www.googleapis.com/auth/plus.login', // solicitando acesso ao profile e ao e-mail do usuário
+            'width': 200,
+            'height': 30,
+            'longtitle': true,
+            'theme': 'dark',
+            'onsuccess': onSignIn,
+        });
+    }
+
 	this.save = function() {
 		this.processando = true;
         DevShareService.objRest.one('/usuario').customPOST(this.data)
